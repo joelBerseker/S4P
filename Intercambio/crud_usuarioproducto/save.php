215dd0e92388b7fdf6fca6deb94a84eb404c51c0
joelBerseker <jatamaria@unsa.edu.pe>
2019-11-20 17:04:52 -0500

    Arreglado lo de intercambio save(usuario)

--- Intercambio/crud_usuarioproducto/save.php
+++ Intercambio/crud_usuarioproducto/save.php
@@ -1,38 +1,44 @@
 <?php
+
 include('../../includes/sesion.php');
+
 include('../../includes/data_base.php');
-$recurso="/Intercambio/save";
+$recurso = "/Intercambio/save";
 //include("../../includes/acl.php");
 ?>
 
 <?php
-if(!empty($user)){
-    if(isset($_POST['save_product'])){
-        $nombre      =$_POST['nombre'];
+if (!empty($user)) {
+    if (isset($_POST['save_product'])) {
+        $nombre      = $_POST['nombre'];
         $description = $_POST['descripcion'];
         $producto = $_POST['producto'];
-            $precio = $_POST['precio'];
-            $estado = $_POST['estado'];   
-        $correo = $user['UsuCor']; 
-        
+        $precio = $_POST['precio'];
+        $estado = $_POST['estado'];
+        $correa = $user['UsuCor'];
+        $queryUser = "SELECT UsuID FROM usuario WHERE UsuCor = '$correa'";
+        echo  "SELECT UsuID FROM usuario WHERE UsuCor = '$correa'";
+        $resultUser = mysqli_query($conn, $queryUser);
+        if (mysqli_num_rows($resultUser) == 1) {
+            $rowUser                = mysqli_fetch_array($resultUser);
+            $idUser                 = $rowUser['UsuID'];
+            $user = $idUser;
+            $query = "INSERT INTO usuario_producto (`UsuProUsuID`,`UsuProProID`, `UsuProPre`, `UsuProEst`, `UsuProDes`,`UsuProNom`) VALUES (?,?, ?, ?, ?, ?)";
+            $stmt = mysqli_prepare($conn, $query);
+            $stmt->bind_param('iidiss', $user, $producto, $precio, $estado, $description, $nombre);
 
-        $user=4;                                                                                                                                     
-        $query = "INSERT INTO usuario_producto (`UsuProUsuID`,`UsuProProID`, `UsuProPre`, `UsuProEst`, `UsuProDes`,`UsuProNom`) VALUES (?,?, ?, ?, ?, ?)";
-        $stmt = mysqli_prepare($conn,$query);
-        $stmt->bind_param('iidiss',$user,$producto,$precio,$estado, $description,$nombre);
-        
-        if(!mysqli_stmt_execute($stmt)){
-            echo "Chanfle, hubo un problema y no se guardo el archivo. ". mysqli_stmt_error($stmt)."<br/>";
-        }      
-        
-        mysqli_stmt_close($stmt);
-        mysqli_close($conn);
-        header("Location: ../");
-    }
-    else{
+            if (!mysqli_stmt_execute($stmt)) {
+                echo "Chanfle, hubo un problema y no se guardo el archivo. " . mysqli_stmt_error($stmt) . "<br/>";
+            }
+
+            mysqli_stmt_close($stmt);
+            mysqli_close($conn);
+            header("Location: ../");
+        } else {
+            header("Location: /S4P/Errores/?m=jodete1");
+        }
+    } else {
         echo "No envio";
     }
-}else{
-    
-}
+} else { }
 ?>
