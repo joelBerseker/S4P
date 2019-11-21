1c94feda445ccc442599693b0172788b3d9f4bdf
joelBerseker <jatamaria@unsa.edu.pe>
2019-11-21 14:28:12 -0500

    Acl de usuarios para que el administrador pueda editar

--- includes/acl_usuario_edit.php
+++ includes/acl_usuario_edit.php
@@ -12,59 +12,63 @@
             $UsuCor=$datos_de_Usuario['UsuCor'];
             $UsuEst=$datos_de_Usuario['UsuEst'];
             $RolID=$datos_de_Usuario['UsuRolID'];
-            if($idUsuario==$id){
-                if($UsuEst==1){
-                    $query_hacia_tabla_Rol="SELECT RolEst FROM ROL WHERE RolID = $RolID";
-                    $respuesta_de_tabla_Rol = mysqli_query($conn,$query_hacia_tabla_Rol);
-                    if(mysqli_num_rows($respuesta_de_tabla_Rol)== 1){
-                        $datos_de_Rol=mysqli_fetch_array($respuesta_de_tabla_Rol);
-                        $RolEst=$datos_de_Rol['RolEst'];
-                        if($RolEst!=0){
-                            $query_de_tabla_Recurso = "SELECT RecID, RecEst FROM RECURSO WHERE RecNom = '$recurso'";
-                            $respuesta_de_tabla_Recurso =  mysqli_query($conn,$query_de_tabla_Recurso);
-                            if(mysqli_num_rows($respuesta_de_tabla_Recurso)== 1 ){
-                                $row = mysqli_fetch_array($respuesta_de_tabla_Recurso);
-                                $RecID=$row['RecID'];
-                                $RecEst=$row['RecEst'];
-                                if($RecEst==1){
-                                    $query_hacia_tabla_Acceso = "SELECT AccEst FROM ACCESO WHERE AccRolID = '$RolID' && AccRecID = '$RecID'";
-                                    $respuesta_de_tabla_Acceso = mysqli_query($conn,$query_hacia_tabla_Acceso);
-                                    if(mysqli_num_rows($respuesta_de_tabla_Acceso)== 1 ){
-                                        $datos_de_Acceso = mysqli_fetch_array($respuesta_de_tabla_Acceso);
-                                        $AccEst=$datos_de_Acceso['AccEst'];
-                                        if($AccEst==1){
-                                            $accederPag=true;
-                                        }else{
-                                            header("Location: /S4P/Errores/index.php?m=El acceso no esta activo");
-                                            echo "el acceso no esta activo";
+            if($RolID!=1){
+                if($idUsuario==$id){
+                    if($UsuEst==1){
+                        $query_hacia_tabla_Rol="SELECT RolEst FROM ROL WHERE RolID = $RolID";
+                        $respuesta_de_tabla_Rol = mysqli_query($conn,$query_hacia_tabla_Rol);
+                        if(mysqli_num_rows($respuesta_de_tabla_Rol)== 1){
+                            $datos_de_Rol=mysqli_fetch_array($respuesta_de_tabla_Rol);
+                            $RolEst=$datos_de_Rol['RolEst'];
+                            if($RolEst!=0){
+                                $query_de_tabla_Recurso = "SELECT RecID, RecEst FROM RECURSO WHERE RecNom = '$recurso'";
+                                $respuesta_de_tabla_Recurso =  mysqli_query($conn,$query_de_tabla_Recurso);
+                                if(mysqli_num_rows($respuesta_de_tabla_Recurso)== 1 ){
+                                    $row = mysqli_fetch_array($respuesta_de_tabla_Recurso);
+                                    $RecID=$row['RecID'];
+                                    $RecEst=$row['RecEst'];
+                                    if($RecEst==1){
+                                        $query_hacia_tabla_Acceso = "SELECT AccEst FROM ACCESO WHERE AccRolID = '$RolID' && AccRecID = '$RecID'";
+                                        $respuesta_de_tabla_Acceso = mysqli_query($conn,$query_hacia_tabla_Acceso);
+                                        if(mysqli_num_rows($respuesta_de_tabla_Acceso)== 1 ){
+                                            $datos_de_Acceso = mysqli_fetch_array($respuesta_de_tabla_Acceso);
+                                            $AccEst=$datos_de_Acceso['AccEst'];
+                                            if($AccEst==1){
+                                                $accederPag=true;
+                                            }else{
+                                                header("Location: /S4P/Errores/index.php?m=El acceso no esta activo");
+                                                echo "el acceso no esta activo";
+                                            }
+                                        }else {
+                                            header("Location: /S4P/Errores/index.php?m=El acceso no existe");
+                                            echo "el acceso no existe";
                                         }
-                                    }else {
-                                        header("Location: /S4P/Errores/index.php?m=El acceso no existe");
-                                        echo "el acceso no existe";
+                                    }else{
+                                        header("Location: /S4P/Errores/index.php?m=El recurso no esta activo");
+                                        echo "el recurso no esta activo";
                                     }
                                 }else{
-                                    header("Location: /S4P/Errores/index.php?m=El recurso no esta activo");
-                                    echo "el recurso no esta activo";
+                                    header("Location: /S4P/Errores/index.php?m=El recurso no existe");
+                                    echo "el recurso no existe";
                                 }
                             }else{
-                                header("Location: /S4P/Errores/index.php?m=El recurso no existe");
-                                echo "el recurso no existe";
+                                header("Location: /S4P/Errores/index.php?m=El rol no esta activo");
+                                echo "el rol no esta activo";
                             }
                         }else{
-                            header("Location: /S4P/Errores/index.php?m=El rol no esta activo");
-                            echo "el rol no esta activo";
+                            header("Location: /S4P/Errores/index.php?m=El rol no existe");
+                            echo "el rol no existe";
                         }
                     }else{
-                        header("Location: /S4P/Errores/index.php?m=El rol no existe");
-                        echo "el rol no existe";
+                        header("Location: /S4P/Errores/index.php?m=El usuario no esta activo");
+                        echo "el usuario no esta activo";
                     }
                 }else{
-                    header("Location: /S4P/Errores/index.php?m=El usuario no esta activo");
-                    echo "el usuario no esta activo";
+                    header("Location: /S4P/Errores/index.php?m=Este no es tu usuario");
+                    echo "este no es tu usuario";
                 }
             }else{
-                header("Location: /S4P/Errores/index.php?m=Este no es tu usuario");
-                echo "este no es tu usuario";
+                $accederPag=true;
             }
         }else{
             header("Location: /S4P/Errores/index.php?m=El usuario no existe");
