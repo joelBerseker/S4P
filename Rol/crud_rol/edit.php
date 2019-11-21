01b6188dc5ef9832b8513b45a77a2ca68019173b
log4x <achavezsa@unsa.edu.pe>
2019-11-21 10:24:45 -0500

    acl de edit usuario en la carpeta includes , formulario actualizado de rol y de usuario

--- Rol/crud_rol/edit.php
+++ Rol/crud_rol/edit.php
@@ -8,19 +8,15 @@
 	include("../../includes/acl.php");
 ?>
 <?php 
-    
     if(isset($_GET['id'])){
         $id = $_GET['id'];
-       
         $query = "SELECT * FROM ROL WHERE RolID = $id";
         $result = mysqli_query($conn,$query);
         if(mysqli_num_rows($result)== 1 ){
             $row = mysqli_fetch_array($result);
             $desciption = $row['RolNom'];
             $estado = $row['RolEst']; 
-           
         }
-        
     }
     if(isset($_POST['update'])){
         $id = $_GET['id'];
@@ -31,47 +27,38 @@
         header("Location: ../");
     }
 ?>
-<?php
-    
-    
-?>
-
-<?php 
-    
-?>
-
 <div class="section2"><br><br>
     <div class="container p-3"></div>
     <div class="row">
         <div class="col-sm-2 col-md-3 col-lg-4 col-xl-4"></div>
         <div class="col-sm-8 col-md-6 col-lg-4 col-xl-4">
             <div class="card card-body">
-            <form action="edit.php?id=<?php echo $_GET['id']?>" method="POST">
-            <div  class="form-group">
-            <label><b>EDITAR ROL</b></label>
-            </div>
-            <div class="form-row form-group ">
-                <div class="col-4"><label>Nombre:</label></div>
-                <div class="col">
-                    <input value="<?php echo $desciption;?>" class="form-control form-control-sm " type="text" name="description1" required></div>
-            </div>
-            <div class="form-row form-group ">
-        <div class="col-4"><label>Seleccione el estado:</label></div>
-        <div class="col">
-        <select name="estado1" class="form-control">
-        <?php
-            if($estado==1){
-        ?>
-            <option value="1" selected> Activo </option>   
-            <option value="0" > Inactivo </option>   
-        <?php
-            }else{
-        ?>
-            <option value="1" > Activo </option> 
-            <option value="0" selected> Inactivo </option>
-        <?php
-            }
-        ?>
+                <form action="edit.php?id=<?php echo $_GET['id']?>" method="POST">
+                <div  class="form-group">
+                    <label><b>EDITAR ROL</b></label>
+                </div>
+                <div class="form-row form-group ">
+                    <div class="col-4"><label>Nombre:</label></div>
+                    <div class="col">
+                        <input value="<?php echo $desciption;?>" class="form-control form-control-sm " type="text" name="description1" required></div>
+                    </div>
+                <div class="form-row form-group ">
+                    <div class="col-4"><label>Seleccione el estado:</label></div>
+                    <div class="col">
+                    <select name="estado1" class="form-control">
+                        <?php
+                            if($estado==1){
+                        ?>
+                        <option value="1" selected> Activo </option>   
+                        <option value="0" > Inactivo </option>   
+                        <?php
+                            }else{
+                        ?>
+                            <option value="1" > Activo </option> 
+                            <option value="0" selected> Inactivo </option>
+                        <?php
+                            }
+                        ?>
 		</select>
         </div>
     </div>
@@ -84,10 +71,6 @@
         <div class="col-sm-2 col-md-3 col-lg-4 col-xl-4"></div>
     </div><br><br><br>
 </div>
-
-
-
-
 <?php
 	include("../../includes/footer.php")
 ?>
