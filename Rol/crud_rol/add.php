192d137d7fe560b35a502ed5ea03517811432c4c
leowi <jhoshep_1999@hotmail.com>
2019-11-22 20:09:57 -0500

    roles xdxd

--- Rol/crud_rol/add.php
+++ Rol/crud_rol/add.php
@@ -5,7 +5,7 @@
   <div class="modal-dialog" role="document">
     <div class="modal-content">
       <div class="modal-header">
-        <h5 class="modal-title" id="exampleModalLabel">AÑADIR ROL</h5>
+        <h5 class="modal-title" id="exampleModalLabel">Añadir Rol</h5>
         <button type="button" class="close" data-dismiss="modal" aria-label="Close">
           <span aria-hidden="true">&times;</span>
         </button>
